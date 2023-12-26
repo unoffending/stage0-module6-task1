7be93c1550498343e04e18ec091aaaef8bed0339
unoffending <109032978+unoffending@users.noreply.github.com>
2023-12-26 22:09:40 +0600

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,28 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    String getDescription() {
+        String paw;
+        if(numberOfPaws == 1) {
+            paw = "paw";
+        } else {
+            paw = "paws";
+        }
+        String fur;
+        if(hasFur == true) {
+            fur = "a";
+        } else {
+            fur = "no";
+        }
+        return "This animal is mostly " + color + 
+        " It has " + numberOfPaws + " " + paw + " and " + fur + " fur.";
+    }
 }
