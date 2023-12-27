d1f4b817133ed5a4f197a429bf4850ac9fee9aa1
unoffending <109032978+unoffending@users.noreply.github.com>
2023-12-27 17:09:50 +0600

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,38 +1,32 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-   private String color;
+
+    private String color;
     private int numberOfPaws;
     private boolean hasFur;
+    
     Animal(String color, int numberOfPaws, boolean hasFur) {
         this.color = color;
         this.numberOfPaws = numberOfPaws;
         this.hasFur = hasFur;
     }
+    
     String getDescription() {
-        String paw;
-        if(numberOfPaws == 1) {
-            paw = "paw";
-        } else {
-            paw = "paws";
-        }
+        String paws;
         String fur;
+        if(numberOfPaws == 1) {
+            paws = "paw";
+        } else {
+            paws = "paws";
+        }
         if(hasFur) {
             fur = "a";
         } else {
             fur = "no";
         }
-        return "This animal is mostly " + color +
-        ". It has " + numberOfPaws + " " + paw + " and " + fur + " fur.";
+        return "This animal is mostly " + color + ". It has " 
+                + numberOfPaws + " " + paws + " and " + fur + " fur.";
     }
-
-    public String getColor() {
-        return color;
-    }
-    public int getNumberOfPaws() {
-        return numberOfPaws;
-    }
-    public boolean getHasFur() {
-        return hasFur;
-    }
+   
 }
