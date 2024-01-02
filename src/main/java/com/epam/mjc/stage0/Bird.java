3c54474254a020f221369202c90f95be88e8604e
unoffending <109032978+unoffending@users.noreply.github.com>
2024-01-02 18:41:21 +0600

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -2,13 +2,53 @@ package com.epam.mjc.stage0;
 
 public class Bird extends Animal {
 
-    Bird() {
-        super("blue", 2, false);
+    public Bird() {
+
+        super.setColor("blue");
+
+        super.setNumberOfPaws(2);
+
+        super.setHasFur(false);
+
     }
-    
+
     @Override
-    protected String getDescription() {
-    return super.getDescription() + " Moreover, it has 2 wings and can fly.";
+    public String getDescription() {
+
+        String colorOne = super.getColor();
+
+        int numberOfPawsOne  = super.getNumberOfPaws();
+
+        String numberOfPawsTwo = null;
+
+        if(numberOfPawsOne == 1) {
+
+            numberOfPawsTwo = "paw";
+
+        } else {
+
+            numberOfPawsTwo = "paws";
+
+        }
+
+        boolean hasFur1 = super.getHasFur();
+
+        String hasFurTwo = null;
+
+        if(hasFur1 == true) {
+
+            hasFurTwo = "a";
+
+        } else {
+
+            hasFurTwo = "no";
+
+        }
+
+        String animal = "This animal is mostly " + colorOne + "." + " It has " + numberOfPawsOne + " " + numberOfPawsTwo + " and " + hasFurTwo + " fur. Moreover, it has 2 wings and can fly.";
+
+        return animal;
+
     }
-    
+
 }
